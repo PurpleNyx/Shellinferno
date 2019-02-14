1cbf86df7a59ce4f6425b6325b197be6a2cd3ccb
PurpleNyx <kvin.ly@gmail.com>
2019-02-14 14:52:24 +0100

    BORDEL SUR TA TABLE

--- shellhardway.sh
+++ shellhardway.sh
@@ -39,7 +39,7 @@ done
 if [ $dos = 2 ] # Cas où le dossier n'existe pas
 	then
 		echo "Le fichier n'existe pas."
-		echo "EXTERMINATE ! EXTERMINATE !"
+		echo "EXTERMINATE ! EXTERMINATE ! BLBLBLBL"
         exit 1
 fi
 
